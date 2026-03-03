2472f519de030b6fb12ae8fb5e72ddf8666c7bce
Kholod13 <vlad13holod@gmail.com>
2026-03-03 20:24:27 +0100

    creating server and tracking dynamic data in URL

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //Node JS Express - Start
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+})
+app.get('/about', (req, res) => {
+    res.send('About us page');
+})
+app.get('/user/:username/:id', (req, res) => {
+    res.send(`User ID: ${req.params.id}, name: ${req.params.username}`);
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () =>{
+    console.log(`Express server on http://localhost:${PORT}`);
+});
